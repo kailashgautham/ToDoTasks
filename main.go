cfa9e4ff320cbe6d8b5003cc174d90519b4a1b25
Kailash Gautham <kailash.gautham@gmail.com>
2022-05-27 15:13:51 +0800

    url query strings

--- main.go
+++ main.go
@@ -1,26 +1,42 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
 	w.Write([]byte("Hello from To-Do Tasks"))
 }
 
-func showTodos(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Showing todos..."))
+func showTodo(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Fprintf(w, "Showing todo %d.", id)
 }
 
 func createTodo(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		w.Header().Set("Allow", "POST")
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
 	w.Write([]byte("Creating todo..."))
 }
 
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
-	mux.HandleFunc("/todo", showTodos)
+	mux.HandleFunc("/todo", showTodo)
 	mux.HandleFunc("/todo/create", createTodo)
 	log.Println("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
